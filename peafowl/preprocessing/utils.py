3e49bb19a085046bd997fa8960d4bc5d8f54771b
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-11-03 15:18:28 +0100

    Change preprocess

--- peafowl/preprocessing/utils.py
+++ peafowl/preprocessing/utils.py
@@ -2,21 +2,24 @@
 
 from typing import List
 
+import pandas as pd
 import spacy
 
 
-def is_not_junk(token: spacy.tokens.token.Token):
+def is_not_junk(token: spacy.tokens.token.Token) -> bool:
     """Test if token is junk."""
     return token.is_stop is False and token.is_punct is False
 
 
-def lemmatizer(data: List) -> List:
-    """Lemmatize a list of articles."""
+def lemmatizer(text: str, nlp: spacy.language.Language) -> List[str]:
+    """Lemmatize one text."""
+    doc = nlp(text)
+    lemma = [token.lemma_ for token in doc if is_not_junk(token)]
+    return lemma
+
+
+def lemmatizer_dataset(data: pd.Series) -> pd.Series:
+    """Lemmatize a Series of articles."""
     nlp = spacy.load("en_core_web_md")
-    lemmatized_articles = []
-    for sentence in data:
-        doc = nlp(sentence)
-        lemma = [token.lemma_ for token in doc if is_not_junk(token)]
-        if lemma:
-            lemmatized_articles.append(lemma)
-    return lemmatized_articles
+    lemmatize_data = data.apply(lambda x: lemmatizer(x, nlp))
+    return lemmatize_data
