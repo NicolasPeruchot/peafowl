630e054fc5193b50b8993338ab5bba6523203267
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-11-04 10:11:58 +0100

    Better ref and hints

--- peafowl/viz/utils.py
+++ peafowl/viz/utils.py
@@ -7,12 +7,15 @@ import tomotopy
 
 
 def prepare_viz_LDA(model: tomotopy.LDAModel) -> pyLDAvis._prepare.PreparedData:
-    """Prepare LDA Model for viz."""
-    topic_term_dists = np.stack([model.get_topic_word_dist(k) for k in range(model.k)])
-    doc_topic_dists = np.stack([doc.get_topic_dist() for doc in model.docs])
-    doc_lengths = np.array([len(doc.words) for doc in model.docs])
+    """Prepare LDA Model for viz.
+
+    Ref: https://github.com/bab2min/tomotopy/blob/main/examples/lda_visualization.py
+    """
+    topic_term_dists: np.ndarray = np.stack([model.get_topic_word_dist(k) for k in range(model.k)])
+    doc_topic_dists: np.ndarray = np.stack([doc.get_topic_dist() for doc in model.docs])
+    doc_lengths: np.ndarray = np.array([len(doc.words) for doc in model.docs])
     vocab = list(model.used_vocabs)
-    term_frequency = model.used_vocab_freq
+    term_frequency: np.ndarray = model.used_vocab_freq
 
     warnings.filterwarnings("ignore")
     prepared_data = pyLDAvis.prepare(
