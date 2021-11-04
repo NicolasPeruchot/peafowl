93321ed0808503ac9e24a7f4270e660acff9faef
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-11-04 14:30:18 +0100

    Add test for junk token

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,18 +1,31 @@
-"""Pass the ci."""
+"""Preprocessing tests."""
 
 import spacy
 
-from peafowl.preprocessing.utils import lemmatizer
+from peafowl.preprocessing.utils import is_not_junk, lemmatizer
 
 
 def test_lemmatizer():
-    """Pass the ci."""
+    """Test the lemmatizer."""
     # given
-    sentence = "This sentence should be lemmatize."
-    expected_output = ["sentence", "lemmatize"]
+    text = "This text should be lemmatize."
+    expected_output = ["text", "lemmatize"]
     nlp = spacy.load("en_core_web_md")
-    doc = nlp(sentence)
+    doc = nlp(text)
     # when
     output = lemmatizer(doc=doc)
+    # then
+    assert expected_output == output
+
+
+def test_is_not_junk():
+    """Test is_not_junk function."""
+    # given
+    text = "Some words are junk!"
+    nlp = spacy.load("en_core_web_md")
+    doc = nlp(text)
+    expected_output = [False, True, False, True, False]
+    # when
+    output = [is_not_junk(token) for token in doc]
     # then
     assert expected_output == output
