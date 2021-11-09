99f1b8288a7c69fa25e8043d91c102ccc46e4b93
NicolasPeruchot <nicolas.peruchot@gmail.com>
2021-11-09 10:30:49 +0100

    Typo

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -8,7 +8,7 @@ from peafowl.preprocessing.utils import is_not_junk, lemmatizer
 def test_lemmatizer():
     """Test the lemmatizer."""
     # given
-    text = "This text should be lemmatize."
+    text = "This text should be lemmatized."
     expected_output = ["text", "lemmatize"]
     nlp = spacy.load("en_core_web_md")
     doc = nlp(text)
